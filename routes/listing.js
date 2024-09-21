2998bbb67bcf9d43886f76f7850aa16beee05aff
Ramraj214 <ramrajnagapure54321@gmail.com>
2024-09-22 00:11:06 +0530

    Add Project Files

--- routes/listing.js
+++ routes/listing.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const wrapAsync = require("../utils/wrapAsync")
-const { isLoggedIn, isOwner, validateListing } = require("../middleware.js")
+const wrapAsync = require("../utils/wrapAsync");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const multer = require('multer');
 const { storage } = require("../cloudConfig.js");
 const upload = multer({ storage });
 const listingController = require("../controllers/listing.js");
 
-// Index Route , create
+// Index Route, Create
 router.route("/")
     .get(wrapAsync(listingController.index))
     .post(
@@ -17,16 +17,19 @@ router.route("/")
         wrapAsync(listingController.createListing)
     );
 
-//New Route
-router.get("/new", isLoggedIn, listingController.renderNewForm);
+// New Route
+router.get("/new", isLoggedIn, (req, res) => {
+    res.locals.currentPage = '/new'; // Set currentPage
+    listingController.renderNewForm(req, res);
+});
 
 router.route("/category/:category")
     .get(wrapAsync(listingController.showListingsByCategory));
 
 router.route("/country")
     .get(wrapAsync(listingController.showListingsByCountry));
-    
-//Show Route ,Delete Route ,Update Route
+
+// Show Route, Delete Route, Update Route
 router.route("/:id")
     .get(wrapAsync(listingController.showListing))
     .put(
@@ -42,12 +45,10 @@ router.route("/:id")
         wrapAsync(listingController.destroyListing)
     );
 
-//Edit Route
-router.get("/:id/edit",
-    isLoggedIn,
-    isOwner,
-    wrapAsync(listingController.renderEditForm)
-);
-
+// Edit Route
+router.get("/:id/edit", isLoggedIn, isOwner, (req, res) => {
+    res.locals.currentPage = '/edit'; // Set currentPage
+    listingController.renderEditForm(req, res);
+});
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -7,21 +7,47 @@ const { saveRedirectUrl } = require("../middleware.js");
 
 const userController = require("../controllers/user.js");
 
-//user routes
-router.route("/signup").
-get(userController.renderSignupForm).
-post(wrapAsync(userController.signUp));
+module.exports.renderSignupForm = (req, res) => {
+    res.render("./users/signup.ejs", { currentPage: '/signup' }); // Pass currentPage
+}
 
-router.route("/login").
-get(userController.renderLoginForm).
-post(saveRedirectUrl,
-    passport.authenticate("local", {
-    failureRedirect: "/login", 
-    failureFlash: true, 
-    }),
-    wrapAsync(userController.login)
-);
+module.exports.signUp = async (req, res) => {
+    try {
+        let { username, email, password } = req.body;
+        const newUser = new User({
+            email,
+            username
+        });
+        const registeredUser = await User.register(newUser, password);
+        req.login(registeredUser, (err) => {
+            if (err) {
+                return next(err);
+            }
+            req.flash("success", "Welcome to Wanderlust!");
+            return res.redirect("/listings");
+        });
+    } catch (err) {
+        req.flash("error", err.message);
+        res.redirect("/signup");
+    }
+}
 
-router.get("/logout", userController.logout);
+module.exports.renderLoginForm = (req, res) => {
+    res.render("./users/login.ejs", { currentPage: '/login' }); // Pass currentPage
+}
 
-module.exports = router;
+module.exports.login = async (req, res) => {
+    req.flash("success", "Welcome back to Wanderlust!");
+    let redirectUrl = res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
+}
+
+module.exports.logout = (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        req.flash("success", "You are logged out!");
+        res.redirect("/listings");
+    });
+}
