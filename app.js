4e45a8cf9ecc7a424a7b4896f3f557023d331f10
Ramraj214 <ramrajnagapure54321@gmail.com>
2024-09-22 01:00:13 +0530

    Add Project Files

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const User = require("./models/user.js");
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
-const currentPathMiddleware = require("../middleware.js")
+const currentPathMiddleware = require("./middleware.js")
 
 const dbUrl = process.env.ATLASDB_URL;
 const secret = process.env.SECRET;
